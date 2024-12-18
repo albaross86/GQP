bdfc24567c608bc2e2092d5c5d642b7746e49b75
albaross86 <enforcer.kz@gmail.com>
2024-12-19 01:38:46 +0300

    исправлен параметр rb в файле processing.py

--- processing.py
+++ processing.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def get_prediction(plotnost, modul_uprugosti, otverditel, epoxidy, temperatura, pov_plotnost,
                        modul_upr_ras, proch_ras, smola, ugol_nashivki, shag_nashivki, plot_nashivki):
-    with open('models/model3.pkl', 'wb') as f:
+    with open('models/model3.pkl', 'rb') as f:
         model = pickle.load(f)
     
     params = np.array([[plotnost], [modul_uprugosti], [otverditel], [epoxidy], [temperatura], [pov_plotnost],
