4f68466422ea85da901523f00c686d250b1da31b
albaross86 <enforcer.kz@gmail.com>
2024-12-18 22:36:18 +0300

    Обновлён файл приложения (пока версия промежуточная), файл веб-страницы, добавлены файлы requirements.txt, processing.py, добавлены исключения gitignore

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+from processing import get_prediction
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["get", "post"])
 def index():
-    return "Hello!"
+    message="Здесь будет рекомендация от нейросети* (дисклеймер: если все параметры введены правильно и приложение отработало без сбоев)"
+    if request.method == "POST":
+        parameter_1 = request.form.get("parameter_1")
+        parameter_2 = request.form.get("parameter_2")
+        
+        predc = get_prediction(parameter_1, parameter_2)
+        message = f"Рекомендуемое соотношение при значениях параметра 1 {parameter_1}, параметра 2 {parameter_2} составляет {predc}"
+        
+        print(predc)
 
+    return render_template("index.html", message=message)
 
 if __name__ == "__main__":
     app.run()
---
+++ processing.py
@@ -0,0 +1,2 @@
+def get_prediction(parameter_1, parameter_2):
+    return (5 * float(parameter_1) + 3 * float(parameter_2))
