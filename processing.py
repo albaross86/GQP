a002c1570fff6987d1679db0aa33d21398bc3844
albaross86 <enforcer.kz@gmail.com>
2024-12-19 02:59:08 +0300

    Внесены минимальные правки в код

--- processing.py
+++ processing.py
@@ -10,6 +10,7 @@ def get_prediction(plotnost, modul_uprugosti, otverditel, epoxidy, temperatura, 
     min_max_scaler = MinMaxScaler()
     params = np.array([plotnost, modul_uprugosti, otverditel, epoxidy, temperatura, pov_plotnost,
                        modul_upr_ras, proch_ras, smola, ugol_nashivki, shag_nashivki, plot_nashivki])
+    params = params.reshape(1, -1)
     params = min_max_scaler.fit_transform(params)
     y_pred = model.predict(params)
     
