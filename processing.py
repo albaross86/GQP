3f44dc0200f5dc47db1f34a4d99d778f92ffc247
albaross86 <enforcer.kz@gmail.com>
2024-12-19 02:30:21 +0300

    попробуем немножко исправить код processing.py в соответствии с блокнотом

--- processing.py
+++ processing.py
@@ -1,14 +1,16 @@
 import pickle
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 def get_prediction(plotnost, modul_uprugosti, otverditel, epoxidy, temperatura, pov_plotnost,
                        modul_upr_ras, proch_ras, smola, ugol_nashivki, shag_nashivki, plot_nashivki):
     with open('models/model3.pkl', 'rb') as f:
         model = pickle.load(f)
     
+    min_max_scaler = MinMaxScaler()
     params = np.array([[plotnost], [modul_uprugosti], [otverditel], [epoxidy], [temperatura], [pov_plotnost],
                        [modul_upr_ras], [proch_ras], [smola], [ugol_nashivki], [shag_nashivki], [plot_nashivki]])
-    params = model.fit_transform(params)
+    params = min_max_scaler.fit_transform(params)
     y_pred = model.predict(params)
     
  
