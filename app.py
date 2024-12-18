3d1d6a0a3114e3cbf4711289f03a4aec3d5169d1
albaross86 <enforcer.kz@gmail.com>
2024-12-19 00:02:13 +0300

    Обновлен файл app.py (добавлено приложение), файл processing.py, добавлены модели в папку models, пока включена одна из моделей, обновлена страница index.html, обновлен файл README

--- app.py
+++ app.py
@@ -8,13 +8,28 @@ app = Flask(__name__)
 def index():
     message="Здесь будет рекомендация от нейросети* (дисклеймер: если все параметры введены правильно и приложение отработало без сбоев)"
     if request.method == "POST":
-        parameter_1 = request.form.get("parameter_1")
-        parameter_2 = request.form.get("parameter_2")
+        plotnost = request.form.get("01_plotnost")
+        modul_uprugosti = request.form.get("02_modul_uprugosti")
+        otverditel = request.form.get("03_otverditel")
+        epoxidy = request.form.get("04_epoxidy")
+        temperatura = request.form.get("05_temperatura")
+        pov_plotnost = request.form.get("06_pov_plotnost")
+        modul_upr_ras = request.form.get("07_modul_upr_ras")
+        proch_ras = request.form.get("08_proch_ras")
+        smola = request.form.get("09_smola")
+        ugol_nashivki = request.form.get("10_ugol_nashivki")
+        shag_nashivki = request.form.get("11_shag_nashivki")
+        plot_nashivki = request.form.get("12_plot_nashivki")
+      
+        predc = get_prediction([plotnost], [modul_uprugosti], [otverditel], [epoxidy], [temperatura], [pov_plotnost],
+                       [modul_upr_ras], [proch_ras], [smola], [ugol_nashivki], [shag_nashivki], [plot_nashivki])
         
-        predc = get_prediction(parameter_1, parameter_2)
-        message = f"Рекомендуемое соотношение при значениях параметра 1 {parameter_1}, параметра 2 {parameter_2} составляет {predc}"
-        
+        message = f"Рекомендуемое соотношение матрица-наполнитель при значениях параметров {plotnost}, {modul_uprugosti}, {otverditel},
+        {epoxidy}, {temperatura}, {pov_plotnost}, {modul_upr_ras}, {proch_ras}, {smola}, {ugol_nashivki}, {shag_nashivki}, {plot_nashivki} составляет {predc}"
+
         print(predc)
 
     return render_template("index.html", message=message)
 
+# if __name__ == "__main":
+#       app.run()
--- processing.py
+++ processing.py
@@ -1,2 +1,15 @@
+import pickle
+import numpy
+
 def get_prediction(parameter_1, parameter_2):
-    return (5 * float(parameter_1) + 3 * float(parameter_2))
+    with open('models/model2.pkl', 'wb') as f:
+        model = pickle.load(f)
+    
+    params = np.array([[plotnost], [modul_uprugosti], [otverditel], [epoxidy], [temperatura], [pov_plotnost],
+                       [modul_upr_ras], [proch_ras], [smola], [ugol_nashivki], [shag_nashivki], [plot_nashivki]])
+    params = model.transform(params)
+    y_pred = model.predict(params)
+    
+ 
+
+    return model.inverse_transform([y_pred])
